2a27e8d7f51da1fe0da20eeb6c5089426a49f0c8
gnkm <genki.matsunaga@gmail.com>
2022-11-11 23:59:00 +0900

    Add `date.sh`

---
+++ scripts/date.sh
@@ -0,0 +1,15 @@
+#! /usr/bin/env bash
+
+# Required parameters:
+# @raycast.schemaVersion 1
+# @raycast.title datesh
+# @raycast.mode inline
+
+# Optional parameters:
+# @raycast.icon 📅
+
+# Documentation:
+# @raycast.description Show today
+# @raycast.author gnkm
+
+ date +"%Y/%m/%d(%a) %H:%M:%S week(%V)"
